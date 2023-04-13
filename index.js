6c4f6edbdcdca104365550f2bec17040e9377f5b
Marcin Król <101599455+Makolojka@users.noreply.github.com>
2023-04-13 17:47:06 +0200

    Lab6 commit

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-const express = require('express');
-const questions = require('./questions').preQuestions;
-const config = require('./config').config;
-
-const app = express();
-
-app.get('/api/questions', (request, response) => {
-    response.send(questions);
-});
-app.listen(config.port, function () {
-    console.info(`Server is running at port 3000`);
-});
+require( 'babel-core/register' );
+require( './app/app');
