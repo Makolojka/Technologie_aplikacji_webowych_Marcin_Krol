6d8bb184e7ba703d41e3b61ffb8fa367926573e8
Marcin Król <101599455+Makolojka@users.noreply.github.com>
2023-05-01 20:39:43 +0200

    Update 2
    Zad 4, 5 i 6

---
+++ blog/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  public counter: number = 0;
+  public title: string = 'blog';
+
+  add() {
+    this.counter++;
+  }
+
+  remove() {
+    this.counter--;
+  }
+}
